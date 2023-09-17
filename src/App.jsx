a4849c117e2c372f3a21acc87c24a47023c05eda
MRMutahir <mutahirkaeem820@gmail.com>
2023-09-17 06:36:13 -0700

    new fri add

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,34 @@ import React, { useState } from 'react'
 import "./App.css"
 function App() {
   const [adddisplay, setdisplay] = useState({ condition: false, value: "add" });
+  const [name, setname] = useState()
+  const [image, setimage] = useState()
+
   let [fri, setfri] = useState([
     { name: "Rock", image: "https://t3.ftcdn.net/jpg/02/43/12/34/360_F_243123463_zTooub557xEWABDLk0jJklDyLSGl2jrr.jpg", desc: "This user is clear " },
     { name: "Ben", image: "https://image.shutterstock.com/image-photo/young-handsome-man-beard-wearing-260nw-1768126784.jpg", desc: "This user is clear " },
     { name: "Harry", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTkr_jAyp3BEuaFiynThKWwosNUAcMFJsUPWXj9_9U&s", desc: "This user is clear " },])
+  let [restList, setrestList] = useState()
   function showmodal() {
     setdisplay({ condition: true, value: "Close" })
   }
-  function closemodal(params) {
+  function closemodal() {
     setdisplay({ condition: false, value: "Add" })
+  }
+
+  function getfri() {
+    let newfri = {
+      name: name,
+      image: image
+    }
+    setfri([...fri, newfri])
+    // console.log(newfri);
+
+    //  let resValue = [ name, image ]
+    // // console.log(...resValue);
+    //  setrestList({name,image})
+    // // setfri([...resValue])
+    //  console.log(restList);
   }
   return (
     <>
@@ -22,9 +41,9 @@ function App() {
       <div className="popapone" >
         {adddisplay.condition === true ?
           (<div>
-            < input type="text" placeholder='name' />
-            <input type="text" placeholder='image' />
-            <button >add</button></div>
+            < input type="text" placeholder='name' onChange={(e) => setname(e.target.value)} />
+            <input type="text" placeholder='image' onChange={(e) => setimage(e.target.value)} />
+            <button onClick={getfri}>add</button></div>
           )
           : console.log('Fals hen')
         }
