8fa69e6b7e9c4bfffc9dbd38457d485b1496589f
MRMutahir <mutahirkaeem820@gmail.com>
2023-09-17 04:59:17 -0700

    first modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./App.css"
 function App() {
-  let users = [
+  const [adddisplay, setdisplay] = useState({ condition: false, value: "add" });
+  let [fri, setfri] = useState([
     { name: "Rock", image: "https://t3.ftcdn.net/jpg/02/43/12/34/360_F_243123463_zTooub557xEWABDLk0jJklDyLSGl2jrr.jpg", desc: "This user is clear " },
     { name: "Ben", image: "https://image.shutterstock.com/image-photo/young-handsome-man-beard-wearing-260nw-1768126784.jpg", desc: "This user is clear " },
-    { name: "Harry", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTkr_jAyp3BEuaFiynThKWwosNUAcMFJsUPWXj9_9U&s", desc: "This user is clear " },]
-  // const [first, setfirst] = useState(second)
-  function addfoo() {
-    console.log("SALAM");
-    let display = {
-      display: true
-    }
-
-
+    { name: "Harry", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTkr_jAyp3BEuaFiynThKWwosNUAcMFJsUPWXj9_9U&s", desc: "This user is clear " },])
+  function showmodal() {
+    setdisplay({ condition: true, value: "Close" })
+  }
+  function closemodal(params) {
+    setdisplay({ condition: false, value: "Add" })
   }
   return (
     <>
+      <div className="addbtn">
+        <button onClick={adddisplay.value === "Close" ? closemodal : showmodal}>
+          {adddisplay.value}
+        </button>
+      </div >
+      <div className="popapone" >
+        {adddisplay.condition === true ?
+          (<div>
+            < input type="text" placeholder='name' />
+            <input type="text" placeholder='image' />
+            <button >add</button></div>
+          )
+          : console.log('Fals hen')
+        }
+      </div>
       <div className='parent'>
-        {users.map((ele) => (
+        {fri.map((ele) => (
           <>
             <div className="user">
               <img src={ele.image} alt="" />
@@ -27,14 +40,6 @@ function App() {
               <button>Select</button>
             </div></>
         ))}</div>
-      <div className="addbtn"><button onClick={addfoo}>add fri</button>
-      </div>
-
-      <div className="popapone" style={{ display: "none" }}>
-        <input type="text" placeholder='name' />
-        <input type="text" placeholder='image' />
-        <button>add</button>
-      </div>
     </>
   )
 }
